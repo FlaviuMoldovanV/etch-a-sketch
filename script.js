329f2c08e085a01c76d14635d5fec23bc701af49
FlaviuMoldovanV <69804853+FlaviuMoldovanV@users.noreply.github.com>
2023-11-08 18:41:08 +0200

    Refactor and add comments

--- script.js
+++ script.js
@@ -3,34 +3,37 @@
 const userInput = document.querySelector("button");
 const gridContainer = document.querySelector("#container");
 
+function createGrid(gridSize) {
 
-userInput.addEventListener("click", () => {
+    gridContainer.innerHTML = ""; // Resets the grid
+    gridContainer.style.border = "1px solid #000";
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-    let gridSize = prompt("Please write a number(The maximum is 100) :");
-
-    if ((gridSize > 100) || (gridSize != parseInt(gridSize))) {
-        alert("Incorrect Value! Please try again");
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement("div");
+        square.className = "square";
+        gridContainer.appendChild(square);
     }
+}
 
-    else {
 
-        gridContainer.innerHTML = "";
-        gridContainer.style.border = "1px solid #000";
-        gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+// To make sure only numbers equal to 100 or lower are allowed
+function handleUserInput() {
+    const gridSize = prompt("Please enter a number (maximum is 100):");
 
-        for (let i = 0; i < gridSize * gridSize; i++) {
-
-            const square = document.createElement("div");
-            square.className = "square";
-            gridContainer.appendChild(square);
-
-        }
-
-        document.addEventListener("mouseover", (e) => {
-            if (e.target.matches(".square")) {
-                e.target.style.backgroundColor = "black";
-            }
-        })
-
+    if (gridSize > 100 || gridSize != parseInt(gridSize)) {
+        alert("Incorrect value! Please try again");
+    } else {
+        createGrid(gridSize);
     }
-})
+}
+
+
+userInput.addEventListener("click", handleUserInput);
+
+
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.matches(".square")) {
+        e.target.style.backgroundColor = "black";
+    }
+});
